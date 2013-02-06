df49e3b93c9aef4b4ca83d4d86785c4fb87d2776
Matthew Jonat <matthew.jonat@gmail.com>
2013-02-06 12:06:20 +0000

    added descriptive comments. about to start working on emailer button.

--- printmail.js
+++ printmail.js
@@ -1,34 +1,32 @@
-//$(document).ready(function() {
-	
-	var itinerary = new Array();
+var itinerary = new Array();
 
-	function printpreview(){
-		
-		//
-		$(".checkboxes").each(function(){
-			if($(this).attr('checked')){
-				// last bit of the string below doesnt push the description :(
-				itinerary.push("<tr><td>Booth #" + $(this).attr('value') + "</td><td>" + $(this).attr('name') + "</td><td>" + $(this).attr('bio') + "</td></tr>");
-			}
-		}); 
-		
-		var arayString = itinerary.toString();
-		var noCommas = arayString.replace(/,/g, '');
-		
-		var printContent = "<table align='center'><tr><td colspan='3' align='center'><h3>Your personalised Itinerary:</h3></td></tr>" + noCommas + "<tr><td></table>"
-		
-		$('body').prepend("<div id='printme' style='display:none'>" + printContent + "</div>");
-		
-		w=window.open();
-		w.document.write($('#printme').html());
-		w.print();
-		w.close();
-		//location.reload();
-		
-		itinerary = [];
-		
-		
-	}
+function printpreview(){
+	
+	$(".checkboxes").each(function(){
+		//get relevant information and push to array
+		if($(this).attr('checked')){
+			itinerary.push("<tr><td>Booth #" + $(this).attr('value') + "</td><td>" + $(this).attr('name') + "</td><td>" + $(this).attr('bio') + "</td></tr>");
+		}
+	}); 
+	
+	var arayString = itinerary.toString();
+	//get rid of commas that are created as a result of the array being converted to a string
+	var noCommas = arayString.replace(/,/g, '');
+	
+	var printContent = "<table align='center'><tr><td colspan='3' align='center'><h3>Your personalised Itinerary:</h3></td></tr>" + noCommas + "<tr><td></table>"
+	
+	//create hidden div within the current document with content ready for print
+	$('body').prepend("<div id='printme' style='display:none'>" + printContent + "</div>");
+	
+	//open new window or tab (depending on browser) and open print dialog then close after print or cancel
+	w=window.open();
+	w.document.write($('#printme').html());
+	w.print();
+	w.close();
+	
+	//reset array if user wants to change itinerary before print
+	itinerary = [];
+}
 	
 	function printpage(){
 		window.print();
@@ -42,16 +40,3 @@
 	function echoArray(){
 		
 	}
-
-/* TO DO!!!!!!
- * 
- * So did a bit of cross browser on this..
- * firefox and opera dont like the buttons after the document.write but my guess is this is because these functions should exist after document.write? I think chrome just magically carries them over...
- * Internet explorer 9 doesnt even do the document.write altho 8 and 7 do but again, dont like the buttons afterwards
- * I think I should probably just not do document.write
- * do the thing where you create a div and then keep it hidden but only print out that section
- * 
- * ---
- * 
- * Also figure out how to get the booth description added to the freeking array!
- */
