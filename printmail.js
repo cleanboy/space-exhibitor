9f50ab3fa42019b8a2feb669203dfdcfd14c85e8
Matthew Jonat <matthew.jonat@gmail.com>
2013-02-06 17:41:04 +0000

    started work on the email button but came to the conclusion that it was best to do it serverside ie PHP. Need to research how best to do this with Modx. Also slight changes to line 7 on printmail.js to make table look better.

--- printmail.js
+++ printmail.js
@@ -4,7 +4,7 @@ function getInfo(){
 	$(".checkboxes").each(function(){
 		//get relevant information and push to array
 		if($(this).attr('checked')){
-			itinerary.push("<tr><td>Booth #" + $(this).attr('value') + "</td><td>" + $(this).attr('name') + "</td><td>" + $(this).attr('bio') + "</td></tr>");
+			itinerary.push("<tr><td valign='top'>Booth #" + $(this).attr('value') + "</td><td valign='top'>" + $(this).attr('name') + "</td><td>" + $(this).attr('bio') + "</td></tr>");
 		}
 	}); 
 	
@@ -12,7 +12,7 @@ function getInfo(){
 	//get rid of commas that are created as a result of the array being converted to a string
 	var noCommas = arayString.replace(/,/g, '');
 	
-	var printContent = "<table align='center'><tr><td colspan='3' align='center'><h3>Your personalised Itinerary:</h3></td></tr>" + noCommas + "<tr><td></table>"
+	var printContent = "<table align='center'><tr><td colspan='3' align='center'><h3>Your personalised Itinerary:</h3></td></tr>" + noCommas + "</table>"
 	return printContent;
 }
 
@@ -34,8 +34,13 @@ function printpreview(){
 	
 function emailMe(){
 	var emailContent = getInfo();
+	var eAddress = $('#emailme').val();
+	var emailDetails = '"mailto:' + eAddress + '?subject=Your Space Expo Itinerary&body=' + emailContent + '"';
 	
-	
+	//matt.jonat@smartershows.com
+	console.log(emailDetails);
+	alert('stop');
+	//parent.location="mailto:you@youremail.com?subject=The subject you want to appear";
 }
 	
 	function backOne(){
